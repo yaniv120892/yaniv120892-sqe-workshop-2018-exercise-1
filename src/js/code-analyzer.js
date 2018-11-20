4eb13a1bdc3d3c040d7cfc962f4bd3a7b1267451
Yaniv Daye <yaniv120892@gmail.com>
2018-11-20 09:52:38 +0000

    Add escodegen support and change model types

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import * as esprima from 'esprima';
+import * as escodegen  from 'escodegen';
 import {Model} from './model_class';
 
 let list_model;
@@ -24,6 +25,18 @@ let dic_function = {
     'Program': parse_program
 };
 
+let dic_types = {
+    'VariableDeclarator': 'variable declarator' ,
+    'ReturnStatement': 'return statement' ,
+    'AssignmentExpression': 'assignment expression' ,
+    'UpdateExpression': 'update expression',
+    'FunctionDeclaration': 'function declaration' ,
+    'VariableDeclaration': 'variable declaration' ,
+    'IfStatement': 'if statement' ,
+    'WhileStatement': 'while statement',
+    'ForStatement': 'for statement',
+}
+
 
 const parseCode = (codeToParse) => {
     return esprima.parseScript(codeToParse, {loc: true});
@@ -38,27 +51,24 @@ function parse_identifier(json_object) {
     return ans;
 }
 function parse_binary_exp(json_object) {
-    let ans = get_details(json_object.left) + ' ' + json_object.operator + ' ' + get_details(json_object.right);
+    let ans = escodegen.generate(json_object);
     return ans;
 }
 function parse_unary_exp(json_object) {
-    let ans = json_object.operator + '' + get_details(json_object.argument);
+    let ans = escodegen.generate(json_object);
     return ans;
 }
 function parse_member_exp(json_object) {
-    let ans = get_details(json_object.object) + '[' + get_details(json_object.property) + ']';
+    let ans = escodegen.generate(json_object);
     return ans;
 }
 function parse_update_exp(json_object) {
-    let operator = json_object.operator;
-    let argument = get_details(json_object.argument);
-    if(json_object.prefix)
-        return operator +''+argument;
-    else
-        return argument +''+operator;
+    let ans = escodegen.generate(json_object);
+    return ans;
+
 }
 function parse_assign_exp(json_object) {
-    let curr_model = new Model(json_object.loc.start.line, json_object.type, get_details(json_object.left),'',  get_details(json_object.right) );
+    let curr_model = new Model(json_object.loc.start.line, dic_types[json_object.type], get_details(json_object.left),'',  get_details(json_object.right) );
     list_model.push(curr_model);
 }
 function parse_exp_stmt(json_object) {
@@ -66,7 +76,7 @@ function parse_exp_stmt(json_object) {
     return ans;
 }
 function parse_return_stmt(json_object) {
-    let curr_model = new Model(json_object.loc.start.line, json_object.type,'','',get_details(json_object.argument));
+    let curr_model = new Model(json_object.loc.start.line, dic_types[json_object.type],'','',get_details(json_object.argument));
     list_model.push(curr_model);
 }
 function parse_block_stmt(json_object) {
@@ -79,8 +89,8 @@ function parse_if_stmt(json_object, inside_else_stmt) {
     let left = get_details(json_object.test.left);
     let right = get_details(json_object.test.right);
     let condition = left + ' ' + operator + ' ' + right;
-    let type = json_object.type;
-    if(inside_else_stmt) type = 'ElseIfStatement';
+    let type = dic_types[json_object.type];
+    if(inside_else_stmt) type = 'else if statement';
     let curr_model = new Model(json_object.loc.start.line,type,'',condition,'');
     list_model.push(curr_model);
     get_details(json_object.consequent);
@@ -89,24 +99,24 @@ function parse_if_stmt(json_object, inside_else_stmt) {
 }
 function parse_while_stmt(json_object) {
     let condition = get_details(json_object.test.left) + ' ' + json_object.test.operator + ' ' + get_details(json_object.test.right);
-    let curr_model = new Model(json_object.loc.start.line, json_object.type,'',condition,'');
+    let curr_model = new Model(json_object.loc.start.line, dic_types[json_object.type],'',condition,'');
     list_model.push(curr_model);
     get_details(json_object.body);
 }
 function parse_for_stmt(json_object) {
     let condition = get_details(json_object.test);
-    let curr_model = new Model(json_object.loc.start.line, json_object.type,'',condition,'');
+    let curr_model = new Model(json_object.loc.start.line, dic_types[json_object.type],'',condition,'');
     list_model.push(curr_model);
     get_details(json_object.init);
     get_details(json_object.update);
     get_details(json_object.body);
 }
 function parse_variable_declarator(json_object) {
-    let curr_model = new Model(json_object.loc.start.line,json_object.type, get_details(json_object.id),'',get_details(json_object.init));
+    let curr_model = new Model(json_object.loc.start.line,dic_types[json_object.type], get_details(json_object.id),'',get_details(json_object.init));
     list_model.push(curr_model);
 }
 function parse_func_decl(json_object) {
-    let curr_model = new Model(json_object.loc.start.line, json_object.type, get_details(json_object.id) ,'','');
+    let curr_model = new Model(json_object.loc.start.line, dic_types[json_object.type], get_details(json_object.id) ,'','');
     list_model.push(curr_model);
     for (let i = 0; i < json_object.params.length; i++) {
         curr_model = new Model(json_object.params[i].loc.start.line, 'variable declaration', get_details(json_object.params[i]), '', '');
--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -4,50 +4,66 @@ import {Model} from '../src/js/model_class';
 
 describe('Declarations',() => {
     it(': variableDeclarator ', () => {
-        assert.deepEqual(viewCode(JSON.stringify(parseCode('let a = 1;'))), [new Model(1, 'VariableDeclarator', 'a', '', 1)]);});
+        assert.deepEqual(viewCode(JSON.stringify(parseCode('let a = 1;'))),
+            [new Model(1, 'variable declarator', 'a', '', 1)]);});
     it(':FunctionDeclaration', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('function b(x){}'))),
-            [new Model(1, 'FunctionDeclaration', 'b', '', ''), new Model(1, 'variable declaration', 'x', '', '')]);});
+            [new Model(1, 'function declaration', 'b', '', ''),
+                new Model(1, 'variable declaration', 'x', '', '')]);});
 });
 describe('Statements',() => {
     it(': ForStatement ', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('for(var i = 0; i < 5; i = i++)\n{x = x + 1;}'))),
-            [new Model(1,'ForStatement','','i < 5',''), new Model(1,'VariableDeclarator','i','',0), new Model(1,'AssignmentExpression','i','','i++'), new Model(2,'AssignmentExpression','x','','x + 1'),]);});
+            [new Model(1,'for statement','','i < 5',''),
+                new Model(1,'variable declarator','i','',0),
+                new Model(1,'assignment expression','i','','i++'),
+                new Model(2,'assignment expression','x','','x + 1'),]);});
     it(': WhileStatement ', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('while(i < 10)\n{i = n-1}'))),
-            [new Model(1,'WhileStatement','','i < 10',''), new Model(2,'AssignmentExpression','i','','n - 1')]);});
+            [new Model(1,'while statement','','i < 10',''),
+                new Model(2,'assignment expression','i','','n - 1')]);});
     it(': IfStatement ', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('if(i < 10)\n{i = i - 7}'))),
-            [new Model(1,'IfStatement','','i < 10',''), new Model(2,'AssignmentExpression','i','','i - 7')]);});
+            [new Model(1,'if statement','','i < 10',''),
+                new Model(2,'assignment expression','i','','i - 7')]);});
     it(': IfStatement followed by else ', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('if(i < 10)\n{i = i - 7}\nelse\n{i = i + 7}'))),
-            [new Model(1,'IfStatement','','i < 10',''), new Model(2,'AssignmentExpression','i','','i - 7'),
-                new Model(4,'AssignmentExpression','i','','i + 7')]);});
+            [new Model(1,'if statement','','i < 10',''),
+                new Model(2,'assignment expression','i','','i - 7'),
+                new Model(4,'assignment expression','i','','i + 7')]);});
     it(': IfStatement and ElseIfStatement ', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('if(i < 10)\n{i = i - 7;}\nelse\nif(x > 10)\n{i = 10;}'))),
-            [new Model(1,'IfStatement','','i < 10',''), new Model(2,'AssignmentExpression','i','','i - 7'), new Model(4,'ElseIfStatement','','x > 10',''), new Model(5,'AssignmentExpression','i','',10)]);});
+            [new Model(1,'if statement','','i < 10',''),
+                new Model(2,'assignment expression','i','','i - 7'),
+                new Model(4,'else if statement','','x > 10',''),
+                new Model(5,'assignment expression','i','',10)]);});
     it(':FunctionDeclaration with ReturnStatement', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('function binarySearch(){\nreturn -1;}'))),
-            [new Model(1, 'FunctionDeclaration', 'binarySearch', '', ''), new Model(2, 'ReturnStatement', '', '', '-1')]);});
+            [new Model(1, 'function declaration', 'binarySearch', '', ''),
+                new Model(2, 'return statement', '', '', '-1')]);});
 });
 describe('Expressions',() => {
     it('ForStatement with UpdateExpression(prefix = false)', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('for(var i = 0; i < 5; i = i++)\n{x = x + 1;}'))),
-            [new Model(1,'ForStatement','','i < 5',''), new Model(1,'VariableDeclarator','i','',0), new Model(1,'AssignmentExpression','i','','i++'), new Model(2,'AssignmentExpression','x','','x + 1'),]);});
+            [new Model(1,'for statement','','i < 5',''), new Model(1,'variable declarator','i','',0), new Model(1,'assignment expression','i','','i++'), new Model(2,'assignment expression','x','','x + 1'),]);});
     it('ForStatement with UpdateExpression(prefix = true)', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('for(var i = 0; i < 5; i = ++i)\n{x = x + 1;}'))),
-            [new Model(1,'ForStatement','','i < 5',''), new Model(1,'VariableDeclarator','i','',0), new Model(1,'AssignmentExpression','i','','++i'), new Model(2,'AssignmentExpression','x','','x + 1'),]);});
+            [new Model(1,'for statement','','i < 5',''),
+                new Model(1,'variable declarator','i','',0),
+                new Model(1,'assignment expression','i','','++i'),
+                new Model(2,'assignment expression','x','','x + 1'),]);});
     it('AssignmentExpression with MemberExpression', () => {
-        assert.deepEqual(viewCode(JSON.stringify(parseCode('x = arr[x] + 1;'))),
-            [new Model(1,'AssignmentExpression','x','','arr[x] + 1'),]);});
+        assert.deepEqual(viewCode(JSON.stringify(parseCode('x = arr[x];'))),
+            [new Model(1,'assignment expression','x','','arr[x]'),]);});
     it('ExpressionStatement', () => {
         assert.deepEqual(viewCode(JSON.stringify(parseCode('let low;\n\n\nlow = 0;'))),
-            [new Model(1,'VariableDeclarator','low','',null), new Model(4,'AssignmentExpression','low','',0)]);});
+            [new Model(1,'variable declarator','low','',null),
+                new Model(4,'assignment expression','low','',0)]);});
 });
 describe('Model',() => {
-    let model_for_test = new Model(1,'ForStatement','','i < 5','');
+    let model_for_test = new Model(1,'for statement','','i < 5','');
     it('get_tr_string', () => {
         assert.equal(model_for_test.get_tr_string(),
-            '<tr><td>1</td><td>ForStatement</td><td></td><td>i < 5</td><td></td></tr>');});
+            '<tr><td>1</td><td>for statement</td><td></td><td>i < 5</td><td></td></tr>');});
 });
 
